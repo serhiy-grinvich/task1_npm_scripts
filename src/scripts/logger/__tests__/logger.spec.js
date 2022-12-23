ead649a26e5de73746288b25174f1a0d567bc46e
serhiy-grinvich <113624514+serhiy-grinvich@users.noreply.github.com>
2022-12-24 01:00:10 +0200

    add

--- src/scripts/logger/__tests__/logger.spec.js
+++ src/scripts/logger/__tests__/logger.spec.js
@@ -5,14 +5,14 @@ it('should return saved logs', () => {
   expect(logger.getLogs()).toEqual([]);
 });
 
-it('should save logs in memory', () => {
-  const logger = createLogger('user login');
-  logger.log('hi user!');
-  expect(logger.getLogs()).toEqual([`log - user login - hi user!`]);
-});
-
 it('should save errors in memory', () => {
   const logger = createLogger('user');
   logger.error('oops! something wrong!');
   expect(logger.getLogs()).toEqual([`error - user - oops! something wrong!`]);
+});
+
+it('should save logs in memory', () => {
+  const logger = createLogger('user login');
+  logger.log('hi user!');
+  expect(logger.getLogs()).toEqual([`log - user login - hi user!`]);
 });
